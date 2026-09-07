ee671f703993f156f34f21c4e900b62d56ad3931
Korand-py <MSOkishev@yandex.ru>
2026-09-07 13:34:13 +0300

    commit 2

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,38 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+void main() {
+  String name = 'Артём';
+  int age = 20;
+  double height = 1.75;
+  bool a = true;
+
+  var score = 95;
+  var language = 'Dart';
+
+  const String appName = 'Lab1';
+  final int startYear = 2026; 
+
+  String? city = null;
+
+  String? nickname = null;
+  String display = nickname ?? 'Аноним';
+
+  print(name);
+  print(age);
+  print(height);
+  print(a);
+
+  print('Привет, $name! Тебе $age лет.');
+  print('Через 5 лет тебе будет, ${age + 5} лет.');
+  print('Рост: ${height} м, студент: $a');
+  print('$language: $score');
+
+  print('$appName staarted in $startYear');
+
+  if (city != null) 
+  {
+    print(city.toUpperCase(),);
+  };
+  print(city?.toUpperCase());
+  print(display);
 }
