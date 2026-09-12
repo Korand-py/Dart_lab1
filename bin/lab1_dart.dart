dfa20c616fef82707f15242419742e41688f84d6
Korand-py <MSOkishev@yandex.ru>
2026-09-12 16:57:38 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -34,4 +34,22 @@ void main() {
   };
   print(city?.toUpperCase());
   print(display);
+
+  // 2.7 Коллекции
+  List<String> fruits = ['Яблоко', 'Банан', "Груша"];
+  fruits.add('Апельсин');
+  print(fruits[0]);
+  print(fruits.length);
+  // Map
+  Map<String, dynamic> person = {'name': 'Макс', 'age': 20};
+  print(person['name']);
+  person['city'] = 'Волжский';
+  // Set
+  Set<int> ids = {1, 2, 4, 1, 2, 5};
+  print(ids);
+  print(ids.length);
+
+  for (var fruit in fruits) {
+    print(fruit);
+  }
 }
