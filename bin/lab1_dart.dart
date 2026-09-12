4a8e2e56960758f333800e911cd1808f429346b0
Korand-py <MSOkishev@yandex.ru>
2026-09-12 18:01:42 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -52,4 +52,81 @@ void main() {
   for (var fruit in fruits) {
     print(fruit);
   }
+  print(greet("Макс"));
+  print(square(5));
+  print(half(10));
+  Pet(name: "Снежок", age: 2);
+  print(repeat("Ха"));
+  print(repeat("Ха", 3));
+
+  // Анонимные функции
+  List<int> numbers = [5, 10, 1, 0, 4, 6, 9];
+  numbers.sort((a, b) => b - a);
+  print(numbers);
+
+  List<String> names = ['Максим', 'Ринат', 'Марк'];
+  List<String> upper = names.map((name) => name.toUpperCase()).toList();
+  print(upper);
+  List<String> longNames = names.where((name) => name.length > 4).toList();
+  print(longNames);
+
+  // Инструкции
+
+  int score2 = 85;
+  String grade;
+  if (score2 >= 90) {
+    grade = 'A';
+  } else if (score2 >= 75) {
+    grade = 'B';
+  } else {
+    grade = 'C';
+  }
+  print(grade);
+  String result = score >= 60 ? 'Сдал': 'Не сдал';
+  print(result);
+
+  // Циклы
+
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+  for (var fruit in fruits) {
+    print(fruit);
+  }
+  int n = 0;
+  while (n < 3) {
+    print(n);
+    n ++;
+  }
+  String day = 'Пн';
+  switch (day) {
+    case 'Сб':
+    case 'Вс':
+      print("Выходной");
+      break;
+    case "Пн":
+      print("Начало недели");
+    default:
+      print("Рабочий день");
+  }
+
+}
+// Функции
+
+String greet(String name) {
+  return 'Привет, $name';
+}
+int square(int x) => x * x;
+double half(double x) => x / 2;
+
+void Pet({required String name, String species = 'кот', int age = 0}) {
+  print('$name - $species, возраст $age');
+}
+
+String repeat(String text, [int times = 2]) {
+  String res = '';
+  for (int i = 0; i < times; i++) {
+    res += text;
+  }
+  return res;
 }
